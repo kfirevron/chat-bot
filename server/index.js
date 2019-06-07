c9ed72d63739e77e07dbab5c63106d6d9b4106a5
kfir evron <kfir432@hotmail.com>
2019-06-07 16:12:22 +0300

    server side done

--- server/index.js
+++ server/index.js
@@ -1,20 +1,32 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+import socket from 'socket.io';
+import express from 'express';
+import http from 'http';
+import ChatBot from './ChatBot';
 
-app.get('/', function (req, res) {
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+const bot = new ChatBot();
+
+app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
+
 io.on('connection', socket => {
     console.log('user connected');
     socket.on('chat message', msg => {
+        let botAnswer = {textMessage: bot.answerToQuestions(msg), userId: "bot", userAvatar: bot.avatar};
         io.emit('chat message', msg);
+        //simulate bot is waiting to answer
+        setTimeout(() => {
+            io.emit('bot answer', botAnswer);
+        }, 1000);
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
 });
-http.listen(3000, () => {
+server.listen(3000, () => {
     console.log('listening on *:3000');
 });
